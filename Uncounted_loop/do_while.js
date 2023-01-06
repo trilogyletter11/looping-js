ad1eb4d287137cb6338b80e4e9f4493cf90e9ad4
trilogyletter <trionofaqoth11@gmail.com>
2023-01-06 16:15:20 +0700

    initial update

---
+++ Uncounted_loop/do_while.js
@@ -0,0 +1,7 @@
+var increment = 1;
+do {
+  document.write("<p>Perulangan ke-" + increment + "</p>");
+  console.log(increment);
+
+  increment++;
+} while (increment <= 10);
---
+++ Uncounted_loop/while.js
@@ -0,0 +1,6 @@
+var increment = 1;
+while (increment <= 10) {
+  document.write("<p>Perulangan ke-" + increment + "</p>");
+  console.log(increment);
+  increment++;
+}
